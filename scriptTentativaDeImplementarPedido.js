6307fb600bdf55b17b6110bfc035ffff0ef61bd6
IvanFerroli <88122854+IvanFerroli@users.noreply.github.com>
2022-01-31 14:54:36 -0300

    projeto, versão de entrega

---
+++ scriptTentativaDeImplementarPedido.js
@@ -0,0 +1,86 @@
+// function selectUnselect(){
+//     const optionButton = document.querySelector(".optionButton")
+//     optionButton.classList.toggle(".optionButton")
+// }
+let itemsSelected = 0;
+let mainCourse;
+let beverage;
+let dessert;
+totalPrice = price01 + price02 + price03
+
+function selectMainCourse(element, mainCourse, price) {
+
+    const selected = document.querySelector(".main-course .option-selected")
+    mainCourse = item01
+    price = price01
+
+    if(selected != null){
+        selected.classList.remove("option-selected")
+    }else{
+        
+        itemsSelected = itemsSelected + 1
+    }
+    
+    element.classList.add("option-selected")
+    
+    
+    validateSelection(); 
+    console.log(itemsSelected)
+}
+
+function selectBeverage(element, selectedBeverage) {
+
+    const selected = document.querySelector(".beverage .option-selected")
+
+    if(selected != null){
+        selected.classList.remove("option-selected")
+    }else{
+        
+        itemsSelected = itemsSelected + 1
+
+    }
+
+    element.classList.add("option-selected")
+    
+    
+    validateSelection();
+    console.log(itemsSelected)
+    
+}
+
+function selectDessert(element, selectedDessert) {
+
+    const selected = document.querySelector(".dessert .option-selected")
+
+
+    if(selected != null){
+        selected.classList.remove("option-selected")
+    }else{
+        
+        itemsSelected = itemsSelected + 1
+
+    }
+
+    element.classList.add("option-selected")
+    
+    
+    validateSelection();
+    console.log(itemsSelected)
+    
+}
+
+function validateSelection (){
+    console.log(itemsSelected)
+    const purchaseButton = document.querySelector(".purchase-button")
+    if(itemsSelected === 3){
+        purchaseButton.classList.remove("button-disabled")
+        purchaseButton.innerTEXT = "Fechar pedido"
+        
+    }else{
+        purchaseButton.classList.add("button-disabled")
+        purchaseButton.innerTEXT = "Selecione 3 itens para fechar o pedido"
+    }
+}
+
+
+
