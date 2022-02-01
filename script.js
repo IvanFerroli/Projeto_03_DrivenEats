341eac1c2962b61ec089c538717fd03425e47240
IvanFerroli <88122854+IvanFerroli@users.noreply.github.com>
2022-01-31 21:13:51 -0300

    É só formatação de código, não mexi no código em si, aceita ai? =D

--- script.js
+++ script.js
@@ -1,82 +1,61 @@
-// function selectUnselect(){
-//     const optionButton = document.querySelector(".optionButton")
-//     optionButton.classList.toggle(".optionButton")
-// }
 let itemsSelected = 0;
 let mainCourse;
 let beverage;
 let dessert;
 
 function selectMainCourse(element) {
+  const selected = document.querySelector(".main-course .option-selected");
 
-    const selected = document.querySelector(".main-course .option-selected")
+  if (selected != null) {
+    selected.classList.remove("option-selected");
+  } else {
+    itemsSelected = itemsSelected + 1;
+  }
 
-    if(selected != null){
-        selected.classList.remove("option-selected")
-    }else{
-        
-        itemsSelected = itemsSelected + 1
-    }
-    
-    element.classList.add("option-selected")
-    
-    
-    validateSelection(); 
-    console.log(itemsSelected)
+  element.classList.add("option-selected");
+
+  validateSelection();
+  console.log(itemsSelected);
 }
 
 function selectBeverage(element, selectedBeverage) {
+  const selected = document.querySelector(".beverage .option-selected");
 
-    const selected = document.querySelector(".beverage .option-selected")
+  if (selected != null) {
+    selected.classList.remove("option-selected");
+  } else {
+    itemsSelected = itemsSelected + 1;
+  }
 
-    if(selected != null){
-        selected.classList.remove("option-selected")
-    }else{
-        
-        itemsSelected = itemsSelected + 1
+  element.classList.add("option-selected");
 
-    }
-
-    element.classList.add("option-selected")
-    
-    
-    validateSelection();
-    console.log(itemsSelected)
-    
+  validateSelection();
+  console.log(itemsSelected);
 }
 
 function selectDessert(element, selectedDessert) {
+  const selected = document.querySelector(".dessert .option-selected");
 
-    const selected = document.querySelector(".dessert .option-selected")
+  if (selected != null) {
+    selected.classList.remove("option-selected");
+  } else {
+    itemsSelected = itemsSelected + 1;
+  }
 
+  element.classList.add("option-selected");
 
-    if(selected != null){
-        selected.classList.remove("option-selected")
-    }else{
-        
-        itemsSelected = itemsSelected + 1
-
-    }
-
-    element.classList.add("option-selected")
-    
-    
-    validateSelection();
-    console.log(itemsSelected)
-    
+  validateSelection();
+  console.log(itemsSelected);
 }
 
-function validateSelection (){
-    console.log(itemsSelected)
-    const purchaseButton = document.querySelector(".purchase-button")
-    if(itemsSelected === 3){
-        purchaseButton.classList.remove("button-disabled")
-        purchaseButton.innerTEXT = "Fechar pedido"
-        
-    }else{
-        purchaseButton.classList.add("button-disabled")
-        purchaseButton.innerTEXT = "Selecione 3 itens para fechar o pedido"
-    }
+function validateSelection() {
+  console.log(itemsSelected);
+  const purchaseButton = document.querySelector(".purchase-button");
+  if (itemsSelected === 3) {
+    purchaseButton.classList.remove("button-disabled");
+    purchaseButton.innerTEXT = "Fechar pedido";
+  } else {
+    purchaseButton.classList.add("button-disabled");
+    purchaseButton.innerTEXT = "Selecione 3 itens para fechar o pedido";
+  }
 }
-
-
